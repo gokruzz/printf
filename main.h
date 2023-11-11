eb4a8442a0fe58a51c58100b9cfb0a7e36565b58
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-11-11 13:12:20 -0800

    moved

--- main.h
+++ main.h
@@ -1,16 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-/* Print Function */
 int _printf(const char *format, ...);
-/* Function for non-custom conversion specifiers */
-int handle_flags(const char *format, int *a);
-int handle_width(const char *format, int *a, va_list list);
-int handle_precision(const char *format, int *a, va_list list);
-int handle_size(const char *format, int *a);
+int _puts(char *c);
+int my_putchar(char c);
 
 #endif
