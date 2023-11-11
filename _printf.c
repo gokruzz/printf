e999854aad25cb3dc1b4bb5decaebb38c6bbd368
gokruzz <johnsonodukoyatemitope@gmail.com>
2023-11-11 06:24:28 -0800

    print function

--- _printf.c
+++ _printf.c
@@ -1,70 +1,46 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_index);
-
+#include <stdarg.h>
 /**
-  * _printf - Print function
-  * @format: format
-  * Return: -1 or y
-  */
+ * _printf - print function
+ * @format: a character string composing of zero or directives
+ * Return: print
+*/
 int _printf(const char *format, ...)
 {
-	int a, x = 0, y = 0;
-	int flags, width, precision, size, buff_index = 0;
+	int print = 0;
+	va_list args;
 
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_start(args, format);
 
-	if (format == NULL)
+	while (*format != '\0')
 	{
-		return (-1);
-	}
-
-	va_start(list, format);
-
-	for (a =  0; format && format[a] != '\0'; a++)
-	{
-		if (format[a] != '%')
+		if (*format != '%')
 		{
-			buffer[buff_index++] = format[a];
-			if (buff_index == BUFF_SIZE)
-			{
-				print_buffer(buffer, &buff_index);
-			}
-			y++;
+			write(1, format, 1);
+			print++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_index);
-			flags = handle_flags(format, &a);
-			width = handle_width(format, &a, list);
-			precision = handle_precision(format, &a, list);
-			size = handle_size(format, &a);
-			++a;
-			x = handle_print(format, &a, list, buffer, flags, width, precision, size);
-			if (x == -1)
+			/* Handle c & s specifiers */
+			switch (*++format)
 			{
-				return (-1);
+				case 'c':
+					print += fprintf(stdout, "%c", va_arg(args, int));
+					break;
+				case 's':
+					print += fprintf(stdout, "%s", va_arg(args, char*));
+					break;
+				default:
+					/* Handle unknown format specifiers */
+					putchar('%');
+					write(1, format, 1);
+					print += 2;
+					break;
 			}
-			y += x;
 		}
+		format++;
 	}
-	print_buffer(buffer, &buff_index);
-	va_end(list);
 
-	return (y);
-}
-/**
-  * print_buffer - prints content of the buffer
-  * @buffer: array of chars
-  * @buff_index: buffer index
-  * Return: void
-  */
-void print_buffer(char buffer[], int *buff_index)
-{
-	if (*buff_index > 0)
-	{
-		write(1, &buffer[0], *buff_index);
-	}
-	*buff_index = 0;
+	va_end(args);
+	return (print);
 }
