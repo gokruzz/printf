c7d13d481ae6a01139bd6060774215152344bfc9
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-11 21:08:13 -0800

    a function that produces output according to a format

--- my_printf.c
+++ my_printf.c
@@ -9,9 +9,10 @@ int _printf(const char *format, ...)
 {
 	int char_count = 0;
 	va_list format_lists;
+
 	va_start(format_lists, format);
 
-	while(*format)
+	while (*format)
 	{
 		if (*format != '%')
 			write(1, format++, 1), chr_count++;
@@ -23,7 +24,8 @@ int _printf(const char *format, ...)
 		}
 		else if (*format == 's')
 		{
-		char* str = va_arg(format_lists, char*);
+		char *str = va_arg(format_lists, char*);
+		
 		for ( ; *str; char_count++, str++)
 		write(1, str, 1);
 		}
@@ -31,13 +33,13 @@ int _printf(const char *format, ...)
 		{
 		int num = va_arg(format_lists, int);
 		if (num < 0)
-		write(1, "-", 1), char_count++, num =- num;
+		write(1, " - ", 1), char_count++, num =- num;
 		for ( ; num; char_count++, num / = 10)
-		write(1, &"0123456789" [num % 10], 1);
+		write(1, &"0123456789"[num % 10], 1);
 		}
 		format++
-        }
+       }
 
-    va_end(format_lists);
-    return char_count;
+   va_end(format_lists);
+   return (char_count);
 }
