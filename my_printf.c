8de0a401aec9a5323bf0a7a17085b13246966634
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-12 05:29:21 +0100

    a function that produces output according to a format

--- my_printf.c
+++ my_printf.c
@@ -1,88 +1,53 @@
 #include "main.h"
-
 /**
- * _printf - ...
- * @format: a character string composing of zero or directives
- *
- * Return: number of characters printed
- */
+ * _printf - prints to the standard output
+ * @format: character or string composing of directives or zero
+ * 
+ * Return: number of character printed
+*/
 int _printf(const char *format, ...)
 {
-    int print_char = 0;
-    va_list fmat_lists;
-
-    if (format == NULL)
-        return (-1);
-    va_start(fmat_lists, format);
-    while (*format)
+    int char_count = 0;
+    va_list format_lists;
+    
+    va_start(format_lists, format);
+    
+    while(*format) 
     {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            print_char++;
-        }
+        if(*format != '%')
+        write(1, format++, 1), char_count++;
+        
         else
         {
             format++;
-            if (*format == '\0')
-                break;
-            if (*format == '%')
+            
+            if(*format == '%')
+            write(1, format++, 1), char_count++;
+            
+            else if(*format == 'c')
             {
-                write(1, format, 1);
-                print_char++;
+                char c = va_arg(format_lists, int); 
+                
+                write(1, &c, 1), char_count++;
             }
-            else if (*format == 'c')
-            {
-                char c = va_arg(fmat_lists, int);
-
-                write(1, &c, 1);
-                print_char++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(fmat_lists, char*);
-                int str_len = 0;
-
-                while (str[str_len] != '\0')
-                    str_len++;
-                write(1, str, str_len);
-                print_char += str_len;
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(fmat_lists, int);
-                int num_len = 0;
-
-                if (num < 0)
+                else if(*format == 's')
                 {
-                    write(1, "-", 1);
-                    print_char++;
-                    num = -num;
+                    char* str = va_arg(format_lists, char*);
+                    for(;*str;char_count++,str++)
+                    write(1, str, 1);
                 }
-
-                if (num == 0)
-                {
-                    write(1, "0", 1);
-                    print_char++;
-                }
-
-                while (num != 0)
-                {
-                    int digit = num % 10 + '0';
-                    write(1, &digit, 1);
-                    num_len++;
-                    num /= 10;
-                }
-
-                while (num_len > 0)
-                {
-                    num_len--;
-                    print_char++;
-                }
-            }
+                    
+                    else if (*format == 'd' || *format == 'i')
+                    {
+                        int num = va_arg(format_lists, int);
+                        if (num < 0) 
+                        write(1, "-", 1), char_count++, num=-num;
+                        for(;num;char_count++,num/=10)
+                        write(1, &"0123456789"[num%10], 1);
+                    }
+                        format++;
         }
-        format++;
     }
-    va_end(fmat_lists);
-    return (print_char);
+    va_end(format_lists);
+    return char_count;
 }
