4d7723204cf3ce516008af6844f0b2fd67dbb603
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-11-11 18:56:46 -0800

    Files Updated

--- main.h
+++ main.h
@@ -3,14 +3,13 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-<<<<<<< HEAD
 #include <string.h>
-=======
 #include <stdlib.h>
->>>>>>> 049d1299e8d00a6ba45f5f0e2b0e3c0dc4561669
 
-int _printf(const char *format, ...);
+int  _printf(const char *format, ...);
 int _puts(char *c);
 int my_putchar(char c);
+void prints_c(char c);
+void prints_s(char *str);
 
 #endif
